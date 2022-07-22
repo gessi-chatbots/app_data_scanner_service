a56a60576ffd8a7c38f7fc9d4e2673a6c89efd48
Quim Motger <jmotger@essi.upc.edu>
2022-07-22 11:54:41 +0200

    add query scrapping + pagination to AlternativeTo

--- ScannerService/AlternativeToParselScrapper.py
+++ ScannerService/AlternativeToParselScrapper.py
@@ -6,13 +6,19 @@ from bs4 import BeautifulSoup
 from ScannerService.Scrapper import Scrapper
 
 import cloudscraper
+import time
 from flask import current_app
 
+from fake_useragent import UserAgent
 
-HOST_HEAD = 'https://web.archive.org/web/https://alternativeto.net/software/'
+
+HOST = 'https://web.archive.org/'
+
+HOST_HEAD = HOST + 'web/https://alternativeto.net/software/'
 HOST_TAIL = '/about/'
 
-HOST_QUERY = 'https://web.archive.org/web/https://alternativeto.net/tag/'
+HOST_QUERY = HOST + 'web/https://alternativeto.net/tag/'
+HOST_QUERY_TAIL = '?sort=likes'
 
 class AlternativeToParselScrapper(Scrapper):
 
@@ -33,21 +39,82 @@ class AlternativeToParselScrapper(Scrapper):
             app_features_list.append(data)
         return app_features_list
 
+    def __queryWebsiteI(self, url, soup, page):
+
+        apps = soup.find_all(itemprop='name')
+
+        app_list = []
+        for app in apps:
+                app_name = app.text
+
+                try:
+
+                    url = HOST + app.get('href')
+                    scraper = cloudscraper.create_scraper() 
+
+                    ua = UserAgent().random
+                    headers = {
+                        'User-Agent': ua,
+                    }
+
+                    req = scraper.get(url, headers=headers)
+
+                    time.sleep(5)  # suspend execution for 5 secs
+
+                    soup = BeautifulSoup(req.text, 'html.parser')
+                    link = soup.find(attrs={'data-link-action': 'AppStores Link'})
+
+                    if link is not None:
+                        if 'itunes' in link.get('href'):
+                            current_app.logger.info('Ignoring ' + app_name + ' (iOS app)')
+                        else:
+                            package = link.get('href').split('id=')[1]
+                            app_list.append({'name': app_name, 'package': package})
+                            current_app.logger.info('Scrapped app: {app_name: \'' + app_name + '\', package = \'' + package + '\'}')
+                    else:
+                        current_app.logger.error('Ignoring ' + app_name + ' (no package was found)')
+
+                except Exception as e: 
+                    print(e)
+                    current_app.logger.error("Network error. App " + app_name + " could not be extracted")
+
+
+        # Pagination is implemented - however, the wayback machine usually does not have pages in their storage
+        try:
+
+            page = page + 1
+
+            next_page = url + '&p=' + str(page)
+            current_app.logger.info("trying pagination to " + next_page)
+            scraper = cloudscraper.create_scraper() 
+            req = scraper.get(next_page)
+
+            if req.status_code != 404:
+
+                soup = BeautifulSoup(req.text, 'html.parser')
+                current_app.logger.info("success! let's go")
+                app_list = app_list + self.__queryWebsiteI(url, soup, page)
+
+            else:
+                current_app.logger.info("No more pages")
+
+        except Exception as e:
+            print(e)
+            current_app.logger.info("No more pages")
+
+        return app_list
+
     def queryWebsite(self, q):
         app_list = []
         for query in q:
-            url = HOST_QUERY + query
-            req = requests.get(url)
 
+            url = HOST_QUERY + query + HOST_QUERY_TAIL
             scraper = cloudscraper.create_scraper() 
             req = scraper.get(url)
 
             soup = BeautifulSoup(req.text, 'html.parser')
 
-            apps = soup.find_all(itemtype='http://schema.org/SoftwareApplication')
-            print(url + " (" + str(req.status_code) + "): " + str(len(apps)) + " apps")
+            #get apps
 
-            for app in apps:
-                print(app
-                    #.get(class_='col-xs-9 ').get('h3').get('a').get('href')
-                    )
+            app_list = app_list + self.__queryWebsiteI(url, soup, 1)
+        return app_list
