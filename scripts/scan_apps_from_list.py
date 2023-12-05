00d4a98fcfe6ebaf6ce8a7423152ceb39952c67a
Quim Motger <jmotger@essi.upc.edu>
2023-12-05 10:28:13 +0100

    iteration

--- scripts/scan_apps_from_list.py
+++ scripts/scan_apps_from_list.py
@@ -17,30 +17,31 @@ apps = json.load(file)
 i = 0
 responses = []
 while i < len(apps):
-    it_apps = []
-    it_apps.append(apps[i])
+    if i >= 18:
+        it_apps = []
+        it_apps.append(apps[i])
 
-    url = "http://127.0.0.1:5500/export-data?api-consumers=true&web-scrapers=true&review_days_old=365"
+        url = "http://127.0.0.1:5500/export-data?api-consumers=true&web-scrapers=true&review_days_old=365"
 
-    try:
-        call_service(url, it_apps, responses)
-    except Exception as e:
         try:
             call_service(url, it_apps, responses)
         except Exception as e:
-            print(e)
-            missed_apps = None
-            with open("../data/scanApps/fail/missed-apps.json") as f1:
-                missed_apps = json.load(f1)
-                missed_apps.append(it_apps[0])
-            with open("../data/scanApps/fail/missed-apps.json", 'w') as outfile:
-                json.dump(missed_apps, outfile)
+            try:
+                call_service(url, it_apps, responses)
+            except Exception as e:
+                print(e)
+                missed_apps = None
+                with open("../data/scanApps/fail/missed-apps.json") as f1:
+                    missed_apps = json.load(f1)
+                    missed_apps.append(it_apps[0])
+                with open("../data/scanApps/fail/missed-apps.json", 'w') as outfile:
+                    json.dump(missed_apps, outfile)
 
-    i += 1
-    if i % 10 == 0 and i > 0:
-        time.sleep(10)
+        i += 1
+        if i % 10 == 0 and i > 0:
+            time.sleep(10)
 
-    perc = i / len(apps) * 100
-    print("Completed " + str(perc) + "% (" + str(i) + "/" + str(len(apps)) + ")")
+        perc = i / len(apps) * 100
+        print("Completed " + str(perc) + "% (" + str(i) + "/" + str(len(apps)) + ")")
 
 print(responses)
